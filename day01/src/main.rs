38bb17041222f2287af6b1065f64988a365d451e
fritsch <filip.fritsch@gmail.com>
2025-12-02 06:10:08 +0100

    Implement part 2 solution for Day 1 navigation challenge

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -22,9 +22,27 @@ fn part1(input: &str) -> i32 {
     return counter;
 }
 
-fn part2(_input: &str) -> String {
-    // TODO: Implement part 2 solution
-    "expected output".to_string()
+fn part2(input: &str) -> i32 {
+    let mut dial = 50;
+        let mut counter = 0;
+        
+        for line in input.lines() {
+            let distance: i32 = line[1..].parse().unwrap();
+            
+            if line.starts_with('L') {
+                if dial == 0 {
+                    counter += distance / 100;
+                } else if distance >= dial {
+                    counter += 1 + (distance - dial) / 100;
+                }
+                dial = (dial - distance).rem_euclid(100);
+            } else {
+                counter += (dial + distance) / 100;
+                dial = (dial + distance).rem_euclid(100);
+            }
+        }
+        
+    return counter;
 }
 
 fn main() {
@@ -56,7 +74,7 @@ L82";
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(part2(EXAMPLE), "expected output");
+        assert_eq!(part2(EXAMPLE), 6);
     }
 
     #[test]
@@ -70,6 +88,6 @@ L82";
     #[ignore]
     fn test_part2_real() {
         let input = include_str!("../../inputs/day01.txt");
-        assert_eq!(part2(input), "your answer here");
+        assert_eq!(part2(input), 5782);
     }
 }
