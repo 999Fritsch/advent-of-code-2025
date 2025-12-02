b7df7ff9a75a69a0ac32e35682db4ae076f32d41
fritsch <filip.fritsch@gmail.com>
2025-12-02 05:54:22 +0100

    Implement part 1 solution for puzzle input

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,30 @@
-use utils::*;
 
-fn part1(input: &str) -> String {
-    // TODO: Implement part 1 solution
-    "Not implemented".to_string()
+fn part1(input: &str) -> i32 {
+    let mut dial: i32 = 50;
+    let mut counter = 0;
+    
+    for line in input.lines() {
+        let distance: i32 = line[1..].parse().unwrap();
+        
+        if line.starts_with('L') {
+            dial -= distance;
+        } else {
+            dial += distance;
+        }
+        
+        dial = dial.rem_euclid(100);
+        
+        if dial == 0 {
+            counter += 1;
+        }
+    }
+    
+    return counter;
 }
 
-fn part2(input: &str) -> String {
+fn part2(_input: &str) -> String {
     // TODO: Implement part 2 solution
-    "Not implemented".to_string()
+    "expected output".to_string()
 }
 
 fn main() {
@@ -21,11 +38,20 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "example input here";
+    const EXAMPLE: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
 
     #[test]
     fn test_part1_example() {
-        assert_eq!(part1(EXAMPLE), "expected output");
+        assert_eq!(part1(EXAMPLE), 3);
     }
 
     #[test]
@@ -37,7 +63,7 @@ mod tests {
     #[ignore]
     fn test_part1_real() {
         let input = include_str!("../../inputs/day01.txt");
-        assert_eq!(part1(input), "your answer here");
+        assert_eq!(part1(input), 962);
     }
 
     #[test]
